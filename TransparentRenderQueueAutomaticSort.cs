1c9d26d6efda11236df8d1c054123f855b2d5e58
TOKIOatoz <tokioatoz1@gmail.com>
2021-06-12 06:16:03 +0900

    Fix class name

--- TransparentRenderQueueAutomaticSort.cs
+++ TransparentRenderQueueAutomaticSort.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeTransparentSortTransparentRenderQueueAutomaticSort : MonoBehaviour
+public class TransparentRenderQueueAutomaticSort : MonoBehaviour
 {
 
     [SerializeField]
